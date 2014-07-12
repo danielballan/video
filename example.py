aeeb3238eb557ec4de9c7ace3e5481e5c409ba9f
danielballan <daniel.b.allan@gmail.com>
2014-07-12 10:49:40 -0500

    Add demuxing example. It compiles.

--- example.py
+++ example.py
@@ -24,15 +24,22 @@ print('Loaded lib {0}'.format(libavcodec))
 ffi.cdef("""
     typedef ... AVCodec;
     typedef ... AVSampleFormat;
-
-    int    check_sample_fmt       (AVCodec *codec, enum AVSampleFormat sample_fmt);
-    int    select_sample_rate     (AVCodec *codec);
-    int    select_channel_layout  (AVCodec *codec);
-    void   audio_encode_example   (const char *filename);
-    void   audio_decode_example   (const char *outfilename, const char *filename);
-    void   video_encode_example   (const char *filename, int codec_id);
-    void   video_decode_example   (const char *outfilename, const char *filename);
-    void   initialize             ();
+    typedef ... AVFormatContext;
+    typedef ... AVStream;
+    typedef ... AVMediaType;
+    
+    int    check_sample_fmt            (AVCodec *codec, enum AVSampleFormat sample_fmt);
+    int    select_sample_rate          (AVCodec *codec);
+    int    select_channel_layout       (AVCodec *codec);
+    void   audio_encode_example        (const char *filename);
+    void   audio_decode_example        (const char *outfilename, const char *filename);
+    void   video_encode_example        (const char *filename, int codec_id);
+    void   video_decode_example        (const char *outfilename, const char *filename);
+    void   initialize                  ();
+    int    demux                       (int argc, char **argv);
+    int    decode_packet               (int *got_frame, int cached);
+    int    open_codec_context          (int *stream_idx, AVFormatContext *fmt_ctx, enum AVMediaType type);
+    int    get_format_from_sample_fmt  (const char **fmt, enum AVSampleFormat sample_fmt);
 """)
 
 libexample = ffi.verify("""
--- src/functions.h
+++ src/functions.h
@@ -9,11 +9,23 @@
 #include <libavutil/samplefmt.h>
 #include <libavformat/avformat.h>
 
-int    check_sample_fmt       (AVCodec *codec, enum AVSampleFormat sample_fmt);
-int    select_sample_rate     (AVCodec *codec);
-int    select_channel_layout  (AVCodec *codec);
-void   audio_encode_example   (const char *filename);
-void   audio_decode_example   (const char *outfilename, const char *filename);
-void   video_encode_example   (const char *filename, int codec_id);
-void   video_decode_example   (const char *outfilename, const char *filename);
-void   initialize             ();
+/* from second example */
+#include <libavutil/imgutils.h>
+#include <libavutil/samplefmt.h>
+#include <libavutil/timestamp.h>
+#include <libavformat/avformat.h>
+
+static AVFormatContext *fmt_ctx = NULL;
+
+int    check_sample_fmt            (AVCodec *codec, enum AVSampleFormat sample_fmt);
+int    select_sample_rate          (AVCodec *codec);
+int    select_channel_layout       (AVCodec *codec);
+void   audio_encode_example        (const char *filename);
+void   audio_decode_example        (const char *outfilename, const char *filename);
+void   video_encode_example        (const char *filename, int codec_id);
+void   video_decode_example        (const char *outfilename, const char *filename);
+void   initialize                  ();
+int    demux                       (int argc, char **argv);
+int    decode_packet               (int *got_frame, int cached);
+int    open_codec_context          (int *stream_idx, AVFormatContext *fmt_ctx, enum AVMediaType type);
+int    get_format_from_sample_fmt  (const char **fmt, enum AVSampleFormat sample_fmt);
