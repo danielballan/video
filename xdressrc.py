d04ba417ab26d2dad6741868e719aaaf2e4c3fc3
Anthony Scopatz <scopatz@gmail.com>
2014-07-12 09:47:51 -0500

    frist cut of xdress

---
+++ xdressrc.py
@@ -0,0 +1,8 @@
+package = 'video'     # top-level python package name
+
+# wrappers for non-standard types (uints, complex)
+extra_types = 'xdress_extra_types'
+
+functions = [
+    ('*example', 'src/functions.*'),
+    ]
